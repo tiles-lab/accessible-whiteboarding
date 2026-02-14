758f14a3d6ad7b944131741d3f041c90f18d53ef
Sheridan Mehta-McKisick <mcksheridan@gmail.com>
2026-02-14 07:43:43 -0500

    Avoid indicating no tags/connectors
    The application doesn't draw attention to an item not having a tag or a connection in the same way that it might emphasize how an item is empty (e.g. new frames are created at a certain size, regardless of their content, and their emptiness is highly apparent).

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -7,23 +7,23 @@ export interface TagsProps {
 const Tags: React.FC<TagsProps> = ({ tags } = { tags: [] }) => {
   return (
     <div className="a11ywb-tags-box">
-      <p>Tags:</p>
+      {tags?.length ?
+        <>
+          <p>Tags:</p>
 
-      {(!tags || tags.length === 0) && <p>There are no tags.</p>}
-      {tags && tags.length > 0 && (
-        <ul className="a11ywb-tags">
-          {tags.map(tag => (
-            <li
-              key={tag.id}
-              className="a11ywb-tag"
-              data-tag-id={tag.id}
-              data-color={tag.color}
-            >
-              {tag.title}
-            </li>
-          ))}
-        </ul>
-      )}
+            <ul className="a11ywb-tags">
+              {tags.map(tag => (
+                <li
+                  key={tag.id}
+                  className="a11ywb-tag"
+                  data-tag-id={tag.id}
+                  data-color={tag.color}
+                >
+                  {tag.title}
+                </li>
+              ))}
+            </ul>
+        </> : null}
     </div>
   );
 };
