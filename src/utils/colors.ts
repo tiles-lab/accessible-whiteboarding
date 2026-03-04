83d5de5b533aa38e5d63e9c6eee1d8d6d310584b
Jeanella Klarys Pascual <dev@jeekapascual.com>
2026-03-04 20:22:25 +0100

    Utils: Add type guard helpers, e.g. isStickyNote, isFrame to typecast items automatically

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,18 +1,18 @@
-import { Item, StickyNote } from "@mirohq/websdk-types";
+import { Item } from "@mirohq/websdk-types";
 import { ColorConfig, StickyNoteColorConfigMap, TagColorConfigMap } from "@models/colors";
-import { ItemType } from "@models/item";
+import { isStickyNote, isTag } from "./items";
 
 export function getColorConfig(item?: Item): ColorConfig | undefined {
     if (!item) {
         return;
     }
     
-    if (item.type === ItemType.Tag) {
+    if (isTag(item)) {
         return TagColorConfigMap[item.id];
     }
 
-    if (item.type === ItemType.StickyNote) {
-        const colorKey = (item as StickyNote).style.fillColor;
+    if (isStickyNote(item)) {
+        const colorKey = item.style.fillColor;
 
         return StickyNoteColorConfigMap[colorKey];
     }
